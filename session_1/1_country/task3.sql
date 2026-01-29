7ee5f17ccd23843be5088ef33d1d5b8b3690c69b
Vriti-Bansal <tnsq0963@leeds.ac.uk>
2026-01-29 22:31:25 +0000

    end

--- session_1/1_country/task3.sql
+++ session_1/1_country/task3.sql
@@ -13,3 +13,9 @@
 -- write your sql code here
 
 SELECT * from countries;
+SELECT COUNT(Country) FROM countries;
+SELECT Continent,COUNT(Country) from countries GROUP BY Continent;
+SELECT Continent,SUM(Population) from countries GROUP BY Continent;
+SELECT Continent,AVG(GDPPerCapita) from countries group by Continent;
+SELECT Continent,COUNT(Country) from countries group by Continent HAVING COUNT(Country)>5;
+SELECT Continent,SUM(Population) from countries group by Continent having SUM(Population)>1000000000;
