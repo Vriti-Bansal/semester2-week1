c10fa397cf0b9e041a4c5b1e6beb177ff30e1517
Vriti-Bansal <tnsq0963@leeds.ac.uk>
2026-01-30 17:45:36 +0000

    end

--- session_2/2_joins/tasks.sql
+++ session_2/2_joins/tasks.sql
@@ -6,7 +6,5 @@
 -- 1. Open SQLite in terminal: sqlite3 social.db
 -- 2. Load this script: .read tasks.sql
 -- 3. Exit SQLite: .exit
-
-SELECT username,content from users join posts ON users.id=posts.user_id where posts.deleted=1;
-SELECT username,content from users join posts on users.id=posts.user_id where users.username like 'a%';
-SELECT username,content from users join posts on users.id=posts.user_id where posts.content like '%small';
+SELECT content from posts WHERE deleted = 1;
+SELECT username from users where username like 'a%'; 
